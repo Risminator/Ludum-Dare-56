0ec3018a32d0369e8fad3b31cf0c8b11d0409f7d
Risminator <vjdronov@yandex.ru>
2024-10-07 19:31:09 +0300

    feat: UI updated

--- scripts/menus/tip.gd
+++ scripts/menus/tip.gd
@@ -2,14 +2,17 @@ extends TextureRect
 
 var tip_index: int = 0
 var max_tips: int = 6
-var tip_length: int = 0
+var tip_length: int = 256
+
+func _ready():
+	texture.region.position = Vector2.ZERO
 
 func _on_left_btn_pressed() -> void:
 	if tip_index > 0:
 		tip_index -= 1
-		#texture.region.position.x = tip_index * tip_length
+		texture.region.position.y = tip_index * tip_length
 
 func _on_right_btn_pressed() -> void:
 	if tip_index < max_tips - 1:
 		tip_index += 1
-		#texture.region.position.x = tip_index * tip_length
+		texture.region.position.y = tip_index * tip_length
